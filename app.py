d9344846414ceda1a1a7a1c83df38b81ae39bec0
jeisoniglesias <101828895+jeisoniglesias@users.noreply.github.com>
2024-05-25 22:23:36 -0500

    init flask application

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def hello_world():
+        return os.getenv("MONGODB_URI", "Hello, World!")
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run()
